ead58e288f87f6e9d1570e2fe0231be08147707b
granadalook <tiven17.jsga@gmail.com>
2023-02-22 17:02:20 -0500

    FIX: Se crea metodo para traer  todos los datos guardados en memoria

--- src/infrastructure/driving-adapters/console/console.ts
+++ src/infrastructure/driving-adapters/console/console.ts
@@ -1,23 +1,23 @@
 import { UserCreatorUseCase } from "../../../application/usecase/userCreator/userCreator";
 import { InmemoryUserRepository } from "../../implementations/inMemory/inMemoryUserRepository";
 import { User } from "../../../domain/entities/user";
+import { UserGetterUseCase } from "../../../application/usecase/UserGetter/userGetter";
 (async () => {
   const inMemoryUserRepo = new InmemoryUserRepository();
-  console.log("Antes", inMemoryUserRepo.userData);
-  const userCreatorUseCase = new UserCreatorUseCase(
-    new InmemoryUserRepository()
-  );
+
+  //Creando usuario
+  const userCreatorUseCase = new UserCreatorUseCase(inMemoryUserRepo);
   const userToCreate: User = {
     name: "dos",
     age: 26,
     username: "dso",
     id: "holas",
   };
-  const res = await userCreatorUseCase.run(userToCreate);
-  console.log("Despues", res);
-  console.log("Array", inMemoryUserRepo.userData);
+  await userCreatorUseCase.run(userToCreate);
+  console.log("Usuario Creado", inMemoryUserRepo.userData);
 
-  /*  const userGetterUseCase = new UserGetterUseCase(new InmemoryUserRepository());
-  const users = await userGetterUseCase.run();
-  console.log(users); */
+  //Opteniendo usuarios
+  const userGetterUseCase = new UserGetterUseCase(inMemoryUserRepo);
+  const usersReturned = await userGetterUseCase.run();
+  console.log("Usuarios Optenidos", usersReturned);
 })();
