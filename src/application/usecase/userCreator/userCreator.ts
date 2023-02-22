514542adac6fa317d9b6d4b29c7e8c149430eff5
granadalook <tiven17.jsga@gmail.com>
2023-02-22 15:33:54 -0500

    FIX: Cambios en final de linea    en .eslintrc.json

--- src/application/usecase/userCreator/userCreator.ts
+++ src/application/usecase/userCreator/userCreator.ts
@@ -1,19 +1,23 @@
-import { User } from "domain/entities/user"
-import { UserRepository } from "domain/repositories/userRepository"
+import { User } from "../../../domain/entities/user";
+import { UserAlredyExistsException } from "../../../domain/exceptions/UserAlredyExistsException";
+import { UserRepository } from "../../../domain/repositories/userRepository";
+import { ExistUserByUserName } from "../../../domain/services/exist-user-name/existUserByUserName";
 
 export class UserCreatorUseCase {
-    private readonly _userRepository: UserRepository
-    private readonly _existUserByUserName: existUserByUserName
+  private readonly _userRepository: UserRepository;
+  private readonly _existUserByUserName: ExistUserByUserName;
 
-    constructor(userRespository: UserRepository) {
-        this._userRepository = userRespository
-        this._existUserByUserName = new ExistUserByUserName
-    }
+  constructor(userRespository: UserRepository) {
+    this._userRepository = userRespository;
+    this._existUserByUserName = new ExistUserByUserName(userRespository);
+  }
 
-    async run(body: User): Promise<User> {
-        const existUser: boolean = await this._existUserByUserName.run(body.username)
-        if (existUser) throw new UserAlredyExistsException()
-        const userCreated: User = await this._userRepository.save(body)
-        return userCreated
-    }
+  async run(body: User): Promise<User> {
+    const existUser: boolean = await this._existUserByUserName.run(
+      body.username
+    );
+    if (existUser) throw new UserAlredyExistsException();
+    const userCreated: User = await this._userRepository.save(body);
+    return userCreated;
+  }
 }
--- src/domain/entities/user.ts
+++ src/domain/entities/user.ts
@@ -1,6 +1,6 @@
 export interface User {
-    id: string
-    name: string
-    username: string
-    age?: number
+  id: string;
+  name: string;
+  username: string;
+  age?: number;
 }
