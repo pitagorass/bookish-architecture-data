597919399224efaa4eabe9f22437ecdf0d10d952
granadalook <tiven17.jsga@gmail.com>
2023-02-23 13:35:44 -0500

    FIX: Se modifica el metodo  para eliminar usuarios

--- src/infrastructure/implementations/inMemory/inMemoryUserRepository.ts
+++ src/infrastructure/implementations/inMemory/inMemoryUserRepository.ts
@@ -23,7 +23,11 @@ export class InmemoryUserRepository implements UserRepository {
     this.userData = users;
     return user;
   }
-  async delete(user: User): Promise<void> {}
+  async delete(user: User): Promise<void> {
+    const users = this.userData.filter((x) => x.id !== user.id);
+    this.userData = users;
+   // return user;
+  }
   async getById(id: string): Promise<User | null> {
     const userFuond = this.userData.find((x) => x.id === id);
     if (userFuond === undefined) {
